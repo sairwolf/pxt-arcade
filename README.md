08d0025b898da6aef7d52b5c161537751a90f307
sairwolf <48264992+sairwolf@users.noreply.github.com>
2025-03-09 13:01:23 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# pxt-arcade
-arcade自訂積木
+#createText
+自訂Arcade 文字積木
+
+## License
+MIT
+
+## Supported targets
+* for PXT/arcade
+（The metadata above is needed for package search.）
+
+```package
+createText=github:sairwolf/pxt-arcade
+```
---
+++ main.ts
@@ -0,0 +1,16 @@
+//% color="#FFAB19" weight=100 icon="\uf08c" block="createText"
+namespace createText {
+	//% block="disp|text %text |fgColor %fg | bgColor %bg"
+	//% fg.min=0 fg.max=15 bg.min=0 bg.max=15
+	//% text.defl="Hello"
+	export function dispText(text: string, fg: number, bg:number) {
+		const font = image.font8;
+		const width = font.charWidth * text.length;
+		const height = font.charHeight;
+		const res = image.create(width, height);
+		res.f ill(bg);
+		res.print(text, 0, 0, fg, font);
+		const sprite = sprites.create(res, SpriteKind.Food);
+		return sprite;
+	}
+}
